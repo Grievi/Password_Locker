8b97741f039fb66cdd9e9ad41214379f924f0b7c
Grievin <grievin.okumu@student.moringaschool.com>
2021-09-03 16:06:39 +0300

    add a new application and python files

---
+++ Password_Locker/user.py
@@ -0,0 +1,18 @@
+class User:
+
+    '''
+    Create an instance of an object or blueprint for users
+    '''
+
+    user_list = []
+
+    def __init__(self,user_name, password):
+        self.user_name = user_name
+        self.password = password
+
+    def save_user(self):
+        '''
+        Save user method saves a new user
+        '''
+
+        User.user_list.append(self)
