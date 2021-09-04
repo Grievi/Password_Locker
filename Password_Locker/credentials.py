c23f6a1cd267445743d675f48586e447c5f0bd70
Grievin <grievin.okumu@student.moringaschool.com>
2021-09-04 23:08:01 +0300

    create username and Password

--- Password_Locker/credentials.py
+++ Password_Locker/credentials.py
@@ -4,5 +4,35 @@ def main():
     while True:
         print("Hello! Welcome to Password locker")
         print('\n')
-        print("Choose a short code to navigate through: to create new user select 'new': To sign in to your account 'sign' or 'q' to quit")
+        print("Choose a short code to navigate through: to create new user select 'nw': To sign in to your account 'sn' or 'q' to quit")
+        short_code = input().lower()
         print('\n')
+
+        if short_code == 'nw':
+            print('Create Username')
+            created_username = input()
+
+
+            print('Create password')
+            created_password = input()
+
+            print('confirm password')
+            confirm_password =input()
+
+        while confirm_password != created_password:
+            print('Password did not match Try again!')
+            print('Enter Your Password')
+            created_username = input()
+            print('confirm password')
+            confirm_password =input()
+
+        else:
+            print(f'Congratulations {created_username}! Account creation was succesful')
+            print('\n')
+            print('Login into your Created Account')
+            print('Enter Username')
+            created_username = input()
+            print('Enter Your Password')
+
+if __name__ == '__main__':
+	main()
