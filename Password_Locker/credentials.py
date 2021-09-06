603b66a6435886ada855c9dcf874b72f9716b273
Grievin <grievin.okumu@student.moringaschool.com>
2021-09-06 14:08:00 +0300

    add beautification effects to the app

--- Password_Locker/credentials.py
+++ Password_Locker/credentials.py
@@ -41,98 +41,100 @@ def display_credentials(user_name):
 
 def main():
     print('')
-    print('-'*100)
+    print('-'*60)
     while True:
         print("\u001b[32;1mHello! Welcome to Password locker\u001b[0m")
         print('\n')
-        print('-'*100)
-        print("Choose a short code to navigate through:\n :To create new user select type -'new'\n :To sign in to your account type '-sn'\n :To create a credential select -'cr'  \n:To display credentials type '-dc'")
-        print (" :or 'q' to quit")
-        print(' Enter your selected choice:\n')
+        print('-'*60)
+        print("Choose a short code to navigate through:\n :To create new user select type '\u001b[32;1m-new\u001b[0m'\n :To sign in to your account type '\u001b[32;1m-sn \u001b[0m'\n :To create a credential select '\u001b[32;1m-cr\u001b[0m'  \n:To display credentials type '\u001b[32;1m -dc\u001b[0m'")
+        print (" :or '\u001b[31;1mq\u001b[0m' to quit")
+        print('\u001b[36;1m Enter your selected choice:\n\u001b[0m')
         short_code = input().lower()
         print('\n')
 
         if short_code == 'new':
-            print('Create Username')
+            print('\u001b[36;1mCreate Username\u001b[0m')
             created_username = input()
 
 
-            print('Create password')
+            print('\u001b[36;1mCreate password\u001b[0m')
             created_password = input()
 
-            print('confirm password')
+            print('\u001b[36;1mconfirm password\u001b[0m')
             confirm_password =input()
 
             save_user(create_user(created_username, created_password))
             print(' ')
-            print('New Account created succesfully')
+            print('\u001b[32;1mNew Account created succesfully\u001b[0m')
 
             while confirm_password != created_password:
-                print('Password did not match Try again!')
-                print('Enter Your Password')
+                print('\u001b[31;1mPassword did not match Try again!\u001b[0m')
+                print('\u001b[36;1mEnter Your Password\u001b[0m')
                 created_username = input()
-                print('confirm password')
+                print('\u001b[36;1mconfirm password\u001b[0m')
                 confirm_password =input()
 
             else:
-                print(f'Congratulations {created_username}!   Account creation was succesful')
+                print(f'\u001b[32;1mCongratulations {created_username}!   Account creation was succesful\u001b[0m')
                 print('\n')
-                print('Login into your Created Account')
-                print('Enter Username')
+                print('\u001b[36;1mLogin into your Created Account\u001b[0m')
+                print('\u001b[36;1mEnter Username\u001b[0m')
                 entered_username = input()
-                print('Enter Your Password')
+                print('\u001b[36;1mEnter Your Password\u001b[0m')
                 entered_password = input()
 
                 while entered_username != created_username or entered_password != created_password:
-                    print('Entered Username or password is invalid')
+                    print('\u001b[36;1mEntered Username or password is invalid\u001b[0m')
                     print('\n')
-                    print('Enter Username')
+                    print('\u001b[36;1mEnter Username\u001b[0m')
                     entered_username = input()
-                    print('Enter Your Password')
+                    print('\u001b[36;1mEnter Your Password\u001b[0m')
                     entered_password = input()
 
 
                 else:
-                    print(f'Welcome! {entered_username} To Your         Lockers Account')
+                    print(f'\u001b[32;1mWelcome! {entered_username} To Your Lockers Account\u001b[0m')
                     print('\n')
 
         elif short_code == 'sn':    
-            print('Welcome To Password locker')
-            print('Enter Username')
+            print('\u001b[32;1mWelcome To Password locker\u001b[0m')
+            print('\u001b[36;1mEnter Username\u001b[0m')
             user_name = input()
-            print('Enter Your Paasword')
+            print('\u001b[36;1mEnter Your Paasword\u001b[0m')
             password = input()
             user_exist = verify_user(user_name, password)
 
             if user_exist == user_name:
                 print(" ")
-                print(f'Welcome {user_name}. Please select an option to continue.')
+                print(f'\u001b[32;1mWelcome {user_name}. Please select an option to continue.\u001b[0m')
 				
                 while True:
                     print("-"*100)
-                    print('Select codes: \n cr-Create a Credential \n dc-Display Credentials \n and ex-Exit')
+                    print('\u001b[32;1mSelect codes: \n cr-Create a Credential \n dc-Display Credentials \n and ex-Exit\u001b[0m')
                     short_code = input().lower()
                     print('\n')
                     if short_code == 'ex':
-                        print('You have logged out')
+                        print('\u001b[36;1mYou have logged out\u001b[0m')
                         break
                     else:
-                        print('Enter Your Username and Password')
+                        print('\u001b[36;1mEnter Your Username and Password\u001b[0m')
         elif short_code == 'cr':
             
             print('')
-            print('Enter your credential details:')
-            print('Enter Website name')
+            print('\u001b[36;1mEnter your credential details:')
+            print('Enter your user_name\u001b[0m')
+            user_name = input()
+            print('\u001b[36;1mEnter Website name\u001b[0m')
             website_name = input()
-            print('Enter Your Account username')
+            print('\u001b[36;1mEnter Your Account username\u001b[0m')
             account_username = input()
-            print('Enter your password')
+            print('\u001b[36;1mEnter your password\u001b[0m')
             password = input()    
 
             # else: 
             save_credential(create_credential(user_name, website_name, account_username, password))
             print('')
-            print(f'User Credential created for : Website name  {website_name}  - Account Name:{account_username}-   Password{password}')
+            print(f'\u001b[33;1mUser Credential created for: username {user_name} : Website name  {website_name}  - Account Name:{account_username}-   Password{password}\u001b[0m')
             print(' ')
         elif short_code == 'dc':
             print('Enter Username')
@@ -144,14 +146,14 @@ def main():
                     print(f'Site Name: {credential.website_name}- Account Name: {credential.account_name} password:{credential.password}')
                     print('  ')
             else:
-                print('Something went Wrong!Try Again')
+                print('\u001b[31;1mSomething went Wrong!Try Again')
                 print('\n')
                 print('\n')
 
         elif short_code == 'q':
                 break
         else:
-            print('Enter a valid code to conitinue')
+            print('\u001b[36;1mEnter a valid code to conitinue\u001b[0m')
             print('-'*100)
 
 
